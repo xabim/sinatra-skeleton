651ce306d61b972f041369376f53578b77cc2bb8
Javier Marti <xabiorama@gmail.com>
2020-03-28 10:11:11 +0100

    Pass tests for request forecast for darksky lib

--- lib/darksky.rb
+++ lib/darksky.rb
@@ -6,5 +6,42 @@ require 'json'
 module DarkSky
   extend Configuration
 
+  class << self
+    # Retrieve the forecast for a given latitude and longitude.
+    #
+    # @param latitude [String] Latitude.
+    # @param longitude [String] Longitude.
+    # @param options [String] Optional parameters. Valid options are `:time` and `:params`
+    #
+    # Example: DarkSky::request_forecast(1,1,{:parameters => { :lang => ["es"] }})
+    def request_forecast(latitude, longitude, options = {})
+      request_url = "https://#{DarkSky.api_endpoint}/forecast/#{DarkSky.api_key}/#{latitude},#{longitude}"
+      request_url << ",#{options[:time]}" if options[:time]
 
+      request_response = request(request_url, options[:parameters])
+
+      request_code = request_response.code.to_i
+      request_code < 400 ? parse_response(request_response.body) : "Error: #{request_code}"
+    end
+
+    private
+
+    def request(url, params = {})
+      uri = URI("#{url}")
+      uri.query = URI.encode_www_form(params) unless params.nil?
+
+      response = connection.request(Net::HTTP::Get.new("#{uri}"))
+    end
+
+    def connection
+      http = Net::HTTP.new("#{DarkSky.api_endpoint}", 443)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http
+    end
+
+    def parse_response(response)
+      JSON.parse(response)
+    end
+  end
 end
